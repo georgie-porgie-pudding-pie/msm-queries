8a5f0589b3472d290e4173120863d0224a4f02c5
georgie-porgie-pudding-pie <gkobalya@chicagobooth.edu>
2025-05-02 20:20:28 +0000

    final changes

--- app/controllers/msm_controller.rb
+++ app/controllers/msm_controller.rb
@@ -5,6 +5,7 @@ class MsmController < ApplicationController
   end
   def actor
     @id = params.fetch(:id)
+    @list_of_characters = Character.where({ :actor_id => @id})
     a = Actor.where({ :id => @id}).at(0)
     @name = a.name
     @dob = a.dob
@@ -13,5 +14,49 @@ class MsmController < ApplicationController
     @created = a.created_at
     @updated = a.updated_at
     render({ :template => "msm_templates/actor"})
+  end
+  def movies
+    @list_of_movies = Movie.all
+    render({ :template => "msm_templates/movies"})
+  end
+  def movie
+    @id = params.fetch(:id)
+    a = Movie.where({ :id => @id}).at(0)
+    d = Director.where({ :id => a.director_id}).at(0)
+    @title = a.title
+    @year = a.year
+    @duration = a.duration
+    @description = a.description
+    @image = a.image
+    @director = d.name
+    @created = a.created_at
+    @updated = a.updated_at
+    render({ :template => "msm_templates/movie"})
+  end
+  def directors
+    @list_of_directors = Director.all
+    render({ :template => "msm_templates/directors"})
+  end
+  def director    
+    @id = params.fetch(:id)
+    @list_of_movies = Movie.where({ :director_id => @id})
+    a = Director.where({ :id => @id}).at(0)
+    @name = a.name
+    @dob = a.dob
+    @bio = a.bio
+    @image = a.image
+    @created = a.created_at
+    @updated = a.updated_at
+    render({ :template => "msm_templates/director"})
+  end
+  def youngest
+    a = Director.where.not({ :dob => nil}).order({ :dob => :asc})
+    @youngest_director = a.at(0)
+    render({ :template => "msm_templates/youngest"})
+  end
+  def eldest
+    a = Director.where.not({ :dob => nil}).order({ :dob => :desc})
+    @eldest_director = a.at(0)
+    render({ :template => "msm_templates/eldest"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   get("/actors", { :controller => "msm", :action => "actors"})
   get("/actors/:id", { :controller => "msm", :action => "actor"})
+  get("/movies", { :controller => "msm", :action => "movies"})
+  get("/movies/:id", { :controller => "msm", :action => "movie"})
+  get("/directors", { :controller => "msm", :action => "directors"})
+  get("/directors/youngest", { :controller => "msm", :action => "youngest"})
+  get("/directors/eldest", { :controller => "msm", :action => "eldest"})
+  get("/directors/:id", { :controller => "msm", :action => "director"})
 end
